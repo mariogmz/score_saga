2b3458513da216011b097db9e36ef8aab9442ec0
Mario Gómez <mariogomezmtz@gmail.com>
2024-08-01 15:43:06 -0600

    Add JTI field to users to validate JWTs authenticity later

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable
+  devise :database_authenticatable, :registerable, :jwt_authenticatable, jwt_revocation_strategy: self
 end
